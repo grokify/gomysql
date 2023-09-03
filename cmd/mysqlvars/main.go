e24ab9dbd53f2917471a60d1afbebcc4d5f449f7
John Wang <johncwang@gmail.com>
2023-09-03 09:21:35 -0700

    reorg: `mysqlvars`: move cli to `cmd/mysqlvars`

---
+++ cmd/mysqlvars/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/go-sql-driver/mysql"
+	gomysql "github.com/grokify/mysqlvars"
+)
+
+func main() {
+	db, err := sql.Open("mysql", DSN())
+	if err != nil {
+		panic(err)
+	}
+
+	vars, err := gomysql.QueryVariables(db)
+	if err != nil {
+		panic(err)
+	}
+
+	err = vars.WriteCSV(os.Stdout)
+	if err != nil {
+		panic(err)
+	}
+	ver, err := gomysql.QueryVersion(db)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(ver)
+
+}
+
+func DSN() string {
+	var hostFlag = flag.String("h", "", "Connect to host.")
+	var portFlag = flag.Int("P", 3306, "Port number to use for connection.")
+	var userFlag = flag.String("u", "", "User for login if not current user.")
+	var passwordFlag = flag.String("p", "", "Password to use when connecting to server")
+
+	flag.Parse()
+
+	cfg := mysql.Config{
+		User:                 *userFlag,
+		Passwd:               *passwordFlag,
+		Net:                  "tcp",
+		Addr:                 *hostFlag + ":" + strconv.Itoa(*portFlag),
+		DBName:               "sys",
+		AllowNativePasswords: true,
+	}
+	return cfg.FormatDSN()
+}

138df96a27a0b28d8838095d45b1624325ce26bf
John Wang <johncwang@gmail.com>
2023-09-09 11:00:37 -0700

    feat: `constants`: add constants for default database names

---
+++ constants.go
@@ -0,0 +1,8 @@
+package gomysql
+
+const (
+	DatabaseInformationSchema = "information_schema"
+	DatabaseMySQL             = "mysql"
+	DatabasePerformanceSchema = "performance_schema"
+	DatabaseSys               = "sys"
+)
